ef91d9df477c58396b9ed6ad954f1079199e5f73
Carina Blair <carinablair@Carina-ZCW-5.local>
2016-10-31 12:17:42 -0400

    added MySQL DB made user class

---
+++ TangledServer/src/main/java/blair/carina/controller/PreferencesController.java
@@ -0,0 +1,7 @@
+package blair.carina.controller;
+
+/**
+ * Created by carinablair on 10/31/16.
+ */
+public class PreferencesController {
+}
---
+++ TangledServer/src/main/java/blair/carina/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package blair.carina.repository;
+
+/**
+ * Created by carinablair on 10/31/16.
+ */
+public class UserRepository {
+}
